7d4837dcfe7b59d2c61fca34146e10a1dfab031d
iamjc93 <jan@goacquirely.com>
2023-12-04 15:35:12 +0800

    Update instance.js

--- src/api/axios/instance.js
+++ src/api/axios/instance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000/';
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.API_DOMAIN : 'http://localhost:8000/';
 
 const instance = axios.create({
     baseURL,
